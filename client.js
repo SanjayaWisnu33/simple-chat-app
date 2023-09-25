d97326cfb0234440196ad06796634333191a43f4
sndff <sandifaisal41@gmail.com>
2023-09-25 17:34:36 +0700

    Nambahin UI dikit, ngubah pesan jadi yang terakhir paling atas, ngilangin pesan duplikat

--- client.js
+++ client.js
@@ -14,18 +14,22 @@ messageInput.addEventListener('keydown', (e) => {
 });
 
 function sendMessage(messageText) {
-    const messageElement = document.createElement('li');
-    messageElement.textContent = `${username}: ${messageText}`;
-    messageElement.classList.add('sender'); // Add the "sender" class to user's messages
-    messages.appendChild(messageElement);
-
-    socket.emit('chat message', messageText);
+    // ini izin urang comment, biar ga double double dari pesan dari sendernya
+    // const messageElement = document.createElement('li');
+    // messageElement.textContent = `${username}: ${messageText}`;
+    // messageElement.classList.add('sender'); // Add the "sender" class to user's messages
+    // messages.appendChild(messageElement);
+    socket.emit('chat message', `${username}: ${messageText}`);
     messageInput.value = '';
 }
 
 socket.on('chat message', (message) => {
-    const messageElement = document.createElement('li');
+    const messageElement = document.createElement('div');
     messageElement.textContent = message;
     messageElement.classList.add('receiver'); // Add the "receiver" class to received messages
-    messages.appendChild(messageElement);
+    messageElement.classList.add('alert'); // Add the "receiver" class to received messages
+    messageElement.classList.add('alert-success'); // Add the "receiver" class to received messages
+    messageElement.classList.add('text-wrap'); // Add the "receiver" class to received messages
+    // messages.appendChild(messageElement); // yang terakhir di bawah
+    messages.prepend(messageElement); // yang terakhir di atas
 });
