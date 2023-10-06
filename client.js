80ba2e143ac4bfa4a74998079bc14f9b1c21a4c8
SanjayaWisnu33 <sanjayawisnu16@gmail.com>
2023-10-06 14:58:19 +0700

    s

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io(); // Assuming you have socket.io installed
+const socket = io(); 
 
 const messages = document.getElementById('messages');
 const messageInput = document.getElementById('message');
@@ -17,27 +17,21 @@ messageInput.addEventListener('keydown', (e) => {
 });
 
 function sendMessage(messageText) {
-    // ini izin urang comment, biar ga double double dari pesan dari sendernya
-    // const messageElement = document.createElement('li');
-    // messageElement.textContent = `${username}: ${messageText}`;
-    // messageElement.classList.add('sender'); // Add the "sender" class to user's messages
-    // messages.appendChild(messageElement);
+    
     socket.emit('chat message', `${username}: ${messageText}`);
     messageInput.value = '';
 }
 
+// Message handling
 socket.on('chat message', (message) => {
     const messageElement = document.createElement('div');
     messageElement.textContent = message;
-    // messageElement.classList.add(`${username}`); // Add the "receiver" class to received messages
+    
     messageElement.classList.add('receiver'); // Add the "receiver" class to received messages
     messageElement.classList.add('alert'); // Add the "receiver" class to received messages
-    // if (document.getElementsByClassName(`${username}`) == username)
-    //     messageElement.classList.add('alert-primary');
-    // else
-    //     messageElement.classList.add('alert-secondary');
+    
     messageElement.classList.add('alert-success'); // Add the "receiver" class to received messages
     messageElement.classList.add('text-wrap'); // Add the "receiver" class to received messages
-    // messages.appendChild(messageElement); // yang terakhir di bawah
+    
     messages.prepend(messageElement); // yang terakhir di atas
 });
